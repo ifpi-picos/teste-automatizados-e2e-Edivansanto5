cdf12a2243f0e42c8108cdb870ac6b46dff09b0a
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-03-23 20:32:08 -0300

    testando cenario3 login com email/matricula invalido

--- tests/Funcionalidade de Login.spec.js
+++ tests/Funcionalidade de Login.spec.js
@@ -3,7 +3,6 @@ const { test, expect } = require('@playwright/test');
 
 
 test('Login no sistema com matricula/email e senha já com cadastros', async ({ page }) => {
-  
 await page.goto('https://refeitorio.picos.ifpi.edu.br/');
 await page.getByText('☰').click();
 await page.getByRole('link', { name: 'Login' }).click();
@@ -13,3 +12,28 @@ await page.getByPlaceholder('Senha').click();
 await page.getByPlaceholder('Senha').fill('06111994');
 await page.getByRole('button', { name: 'Login' }).click();
 });
+
+test(' Login no sistema com email/matrícula senha sem cadastro',async({page})=>{
+await page.goto('https://refeitorio.picos.ifpi.edu.br/');
+await page.getByText('☰').click();
+await page.getByRole('link', { name: 'Login' }).click();
+await page.getByPlaceholder('Usuário').click();
+await page.getByPlaceholder('Usuário').fill('2018tads0987');
+await page.getByPlaceholder('Senha').click();
+await page.getByPlaceholder('Senha').fill('qualquersenha');
+await page.getByRole('button', { name: 'Login' }).click();
+await page.getByText('× Por favor, entre com um Matricula e senha corretos. Note que ambos os campos d').click();
+});
+
+test('Login com matricula/e-mail inválidos',async({page})=>{
+    await page.goto('https://refeitorio.picos.ifpi.edu.br/');
+    await page.getByText('☰').click();
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByPlaceholder('Usuário').click();
+    await page.getByPlaceholder('Usuário').fill('2190260909909ds');
+    await page.getByPlaceholder('Senha').click();
+    await page.getByPlaceholder('Senha').fill('423424344');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByText('× Por favor, entre com um Matricula e senha corretos. Note que ambos os campos d').click();
+});
+
