e8bdf79714e645bad4c0dbed65663e999f0dd35d
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-03-23 21:11:11 -0300

    finalizando os teste de cenario de tela de login

--- tests/Funcionalidade de Login.spec.ts
+++ tests/Funcionalidade de Login.spec.ts
@@ -37,3 +37,35 @@ test('Login com matricula/e-mail inválidos',async({page})=>{
     await page.getByText('× Por favor, entre com um Matricula e senha corretos. Note que ambos os campos d').click();
 });
 
+test(' Login com senha inválida',async({page})=>{
+await page.goto('https://refeitorio.picos.ifpi.edu.br/');
+await page.getByText('☰').click();
+await page.getByRole('link', { name: 'Login' }).click();
+await page.getByPlaceholder('Usuário').click();
+await page.getByPlaceholder('Usuário').fill('20191tads0326');
+await page.getByPlaceholder('Senha').click();
+await page.getByPlaceholder('Senha').fill('123456');
+await page.getByRole('button', { name: 'Login' }).click();
+await page.getByText('× Por favor, entre com um Matricula e senha corretos. Note que ambos os campos d').click();
+});
+
+test('Login com  campo da senha vazios',async({page})=>{
+await page.goto('https://refeitorio.picos.ifpi.edu.br/');
+await page.getByText('☰').click();
+await page.getByRole('link', { name: 'Login' }).click();
+await page.getByPlaceholder('Usuário').click();
+await page.getByPlaceholder('Usuário').fill('20191tads0326');
+await page.getByPlaceholder('Senha').click();
+await page.getByRole('button', { name: 'Login' }).click();
+await page.getByRole('button', { name: 'Login' }).click();
+await page.locator('div').filter({ hasText: 'Login' }).nth(4).click();
+})
+
+test('Login com  campo da Matricula/email vazios',async({page})=>{
+  await page.goto('https://refeitorio.picos.ifpi.edu.br/');
+  await page.getByText('☰').click();
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByPlaceholder('Senha').click();
+  await page.getByPlaceholder('Senha').fill('1284959');
+  await page.getByRole('button', { name: 'Login' }).click();  
+})
